984467560b0450f1cccb57aff7d266bafefeff30
prabhath <prabhathkiran20@gmail.com>
2015-12-21 23:53:48 -0600

    Add test cases for POST and change some in previous tests

--- lists/tests.py
+++ lists/tests.py
@@ -7,7 +7,6 @@ from django.template.loader import render_to_string
 
 # Create your tests here.
 class HomePageTest(TestCase):
-
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
         self.assertEquals(found.func, home_page)
@@ -16,9 +15,24 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string("home.html")
-        self.assertEquals(response.content.decode(), expected_html)
+        self.assertEqual(response.content.decode(), expected_html)
 
-        """ this is bad as we testing constants """
-        #self.assertTrue(response.content.startswith(b'<html>'))
-        #self.assertIn(b'<title>To-Do lists</title>', response.content)
-        #self.assertTrue(response.content.endswith(b'</html>'))
+        """ this is bad as we are testing constants """
+
+        # self.assertTrue(response.content.startswith(b'<html>'))
+
+        # self.assertIn(b'<title>To-Do lists</title>', response.content)
+
+        # self.assertTrue(response.content.endswith(b'</html>'))
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = "POST"
+        request.POST["item_text"] = "A new list item"
+
+        response = home_page(request)
+
+        self.assertIn("A new list item", response.content.decode())
+
+        expected_html = render_to_string("home.html", {'new_item_text': 'A new list item'})
+        self.assertEqual(response.content.decode(), expected_html)
